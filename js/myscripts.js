7d0e8d0d2cfd689340d2e7bd87054c0c2abd03a8
melnichenko1996 <djek678@mail.ru>
2017-02-01 22:03:35 +0200

    next commit

---
+++ js/myscripts.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function() {
+
+var header = $('header');
+header.hide();
+
+function func() {
+	header.fadeIn(4000);
+}
+setTimeout(func, 3000);
+
+/*$('img[src*=prapor]').hide(5000).show(5000);
+
+var img = $('img[src*=prapor]');
+
+img.click(function(){
+	$('img[src*=prapor]').width('30%');
+});
+
+img.dblclick(function(){
+	$('img[src*=prapor]').width('60%');
+});
+*/
+});
