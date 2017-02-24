c17244f4d826543c85ca1332aa52f389c94b6b2d
melnichenko1996 <djek678@mail.ru>
2017-02-24 07:22:55 +0200

    next commit

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,13 +1,5 @@
 ﻿$(document).ready(function() {
 
-var header = $('header');
-header.hide();
-
-function func() {
-	header.fadeIn(4000);
-}
-setTimeout(func, 3000);
-
 /*$('img[src*=prapor]').hide(5000).show(5000);
 
 var img = $('img[src*=prapor]');
